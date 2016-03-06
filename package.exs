d8eed4d8417b94bc753f01287fcee8e2edf7dc97
Duncan McGreggor <oubiwann@gmail.com>
2016-03-06 14:39:13 -0600

    Removed old EXPM references.

--- package.exs
+++
@@ -1,7 +0,0 @@
-Expm.Package.new(
-  name: "lsci",
-  description: "Scientific Computing on the Erlang VM - An LFE Wrapper Library for SciPy, NumPy, and matplotlib",
-  version: "0.0.1",
-  keywords: ["LFE", "Lisp", "Library", "API", "Python", "Scientific Computing"],
-  maintainers: [[name: "Duncan McGreggor", email: "oubiwann@lfe.io"]],
-  repositories: [[github: "lfex/lsci"]])
